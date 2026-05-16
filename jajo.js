3e4e07dabf4bf8e9047aaeab6c5b851bcece7b7e
joviah-amali <joviahkakwezi89@gmail.com>
2026-05-16 10:05:43 +0300

    Add files via upload

---
+++ jajo.js
@@ -0,0 +1,52 @@
+
+window.alert(`Kitende Burger Hub Come one come all. `)
+window.alert(`Kitende Burger Hub Come one come all. `)
+
+
+function Clickme(){
+    document.getElementById("paragraph").innerHTML="Your Order"}
+
+   // const form = document.getElementById(`form`);
+    //const form = document.getElementById(`Your Name`);
+    //const form = document.getElementById(`email`);
+    //const form = document.getElementById(`Message`);
+
+    //form.addEventListener(`submit`, e=>{
+        //preventDefault prevents the form from submitting because we want to validate out inputs
+       // e.preventDefault();
+
+        //validateInputs();
+   // });
+
+   // const setError = (element, message) =>{
+        //so here we check if the yourname is an empty string=set error for that we'll write a function wic receives an html element
+        //and an error msg as a parameters 
+        //remove success class if present so that the red is add to the input field
+        //error class input with display message put in parameters
+        //const inputtype = element.parentElement;
+        //const errorDisplay = inputtype.querySelector(`.error`);
+
+
+        //errorDisplay.innerText = message;
+        //inputtype.classList.add(`error`);
+        // inputtype.classList.remove(`success`);
+    //}
+
+    //const validateInputs = () => {
+        //trim function removes all the white spaces that a string has
+        //const YourNameValue = YourNamevalue.trim();
+       // const email = email.value.trim();
+        //const MessageValue = Message.value.trim();
+
+        //am adding validation function on each value
+
+       // if(YourNameValue === ``){ 
+            //setError(YourName, `YourName is required`)
+        //}else{
+            //setSuccess(YourName);
+        //}
+
+
+   // };
+
+
